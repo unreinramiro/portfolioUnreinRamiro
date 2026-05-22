712dc445d1f8bb3594f81562c80176b262d2490c
Ramiro Unrein <unreinramiro2000@gmail.com>
2026-05-22 15:31:31 -0300

    Se creo el componente AssignatureCards.jsx y se le dio disenio

--- plantillafullstack.client/src/pages/Studies/Assignatures/AssignatureCard.jsx
+++ plantillafullstack.client/src/pages/Studies/Assignatures/AssignatureCard.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from './AssignatureCard.module.css'
 
 const AssignatureCard = () => {
   return (
-    <div>AssignatureCard</div>
+    <div className={`container ${styles.assignatureCardContainer}`}>
+        <div className='row'>
+            <div className='col-8'>
+                <p>Programacion I</p>
+            </div>
+            <div className='col-4 text-end'>
+                <b className='text-warning'>En curso</b>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-8'>
+                <b>Nota primer parcial: </b>
+            </div>
+            <div className='col-4 text-end'>
+                <b>9.20</b>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-8'>
+                <b>Nota segundo parcial: </b>
+            </div>
+            <div className='col-4 text-end'>
+                <b> - </b>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-8'>
+                <b>Promedio: </b>
+            </div>
+            <div className='col-4 text-end'>
+                <b>9.20</b>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- plantillafullstack.client/src/pages/Studies/Assignatures/AssignaturesModal.jsx
+++ plantillafullstack.client/src/pages/Studies/Assignatures/AssignaturesModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './AssignaturesModal.module.css'
 import SearchBar from '../../../components/SearchBar/SearchBar'
+import AssignatureCard from './AssignatureCard'
 
 const AssignaturesModal = ({ onClose }) => {
   return (
@@ -10,10 +11,20 @@ const AssignaturesModal = ({ onClose }) => {
             onClick={e => e.stopPropagation()}  // evita cerrar al clickear dentro
         >
             <h3 className='text-white text-center'>Materias</h3>
-            <div className='container'>
+            <div className='container d-flex flex-column gap-3'>
                 <div className='row'>
                     <div className='col-12'>
                         <SearchBar />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h4>Primer Año</h4>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12'>
+                        <AssignatureCard />
                     </div>
                 </div>
             </div>
