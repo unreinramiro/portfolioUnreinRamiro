92ef180b49997a052b867a40d728ef41aa271e32
Ramiro Unrein <unreinramiro2000@gmail.com>
2026-07-29 15:09:33 -0300

    Se agrego la parte ProfileAdm.jsx y se esta dando forma

--- plantillafullstack.client/src/components/admin/profile/ProfileAdm.jsx
+++ plantillafullstack.client/src/components/admin/profile/ProfileAdm.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styles from './ProfileAdm.module.css'
+import axiosInstance from '../../../services/api'
+import { FaPen } from "react-icons/fa";
 
 const ProfileAdm = () => {
+
+      const [profile, setProfile] = useState({});
+      const [formProfile, setFormProfile] = useState({
+        profileName : '',
+        profileSurname: '',
+        profileDesc: '',
+        profileImg: ''
+      });
+      const [editMode, setEditMode] = useState(false);
+
+      useEffect(() => {
+          const fetchProfile = async () => {
+              try {
+                  const response = await axiosInstance.get('profile'); // Realiza una solicitud GET a /api/products
+                  setProfile(response.data); // Actualiza el estado con los datos obtenidos
+                  console.log('Profile obtenido:', response.data);
+              } catch (err) {
+                  console.error("Error al obtener el profile", err); // Muestra el error en consola
+              }
+          };
+
+          fetchProfile();
+      }, []);
+
+      const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormProfile({...formProfile, [e.target.name] : e.target.value})
+      };
+
   return (
-    <div>ProfileAdm</div>
+        <div className={`container ${styles.presentationContainer}`}>
+          <div className="row h-100 align-items-center">
+                <div className="col-lg-12 text-center">
+                    <div className={styles.imageAndName}>
+                        <div className={styles.fotoCv}>
+                            <img src={`/src/assets/${profile.prO_IMG}`} />
+                        </div>
+                        <h1 className="text-white">{profile.prO_NAME} {profile.prO_SURNAME}</h1>
+                        <div>
+                            <p className="text-white text-opacity-50 fs-5">Desarrollador Full Stack</p>
+                            <b className="text-white text-opacity-50 fs-5">React JS / .NET Core / SQL Server</b>
+                        </div>
+                        <div>
+                          <button
+                              className={styles.editButton}
+                              onClick={() => setEditMode(true)}
+                          >
+                              Editar
+                          </button>
+                        </div>
+                    </div>
+                </div>
+          </div>
+        </div>
   )
 }
 
--- plantillafullstack.client/src/pages/Dashboard/Dashboard.jsx
+++ plantillafullstack.client/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Dashboard.module.css'
+import ProfileAdm from '../../components/admin/profile/ProfileAdm'
+import ProjectsAdm from '../../components/admin/projects/ProjectsAdm'
+import StudiesAdm from '../../components/admin/studies/StudiesAdm'
+import AssignaturesAdm from '../../components/admin/assignatures/AssignaturesAdm'
+import TechAdm from '../../components/admin/technologies/TechAdm'
 
 const Dashboard = () => {
+
+    const [activeTab, setActiveTab] = useState("Profile");
+    const [session, setSession] = useState("on");
+
   return (
     <section className={styles.dashboardSection}>
-        
+        <div className={`container ${styles.dashboardContainer}`}>
+            <div className='row w-100'>
+                <div className='col-3'>
+                    <div className={`row ${styles.titleContainer}`}>
+                        <h3 className='text-white text-center'>Dashboard</h3>
+                    </div>
+                    <div className={`row ${styles.buttonsContainer}`}>
+                        <button 
+                            onClick={() => setActiveTab("Profile")}
+                            className={`${styles.sidebarButton} ${
+                            activeTab === "Profile" ? styles.active : ""}`}
+                            >Profile</button>
+                        <button 
+                            onClick={() => setActiveTab("Projects")}
+                            className={`${styles.sidebarButton} ${
+                            activeTab === "Projects" ? styles.active : ""}`}
+                            >Projects</button>
+                        <button 
+                            onClick={() => setActiveTab("Studies")}
+                            className={`${styles.sidebarButton} ${
+                            activeTab === "Studies" ? styles.active : ""}`}>Studies</button>
+                        <button 
+                            onClick={() => setActiveTab("Assignatures")}
+                            className={`${styles.sidebarButton} ${
+                            activeTab === "Assignatures" ? styles.active : ""}`}>Assignatures</button>
+                        <button 
+                            onClick={() => setActiveTab("Technologies")}
+                            className={`${styles.sidebarButton} ${
+                            activeTab === "Technologies" ? styles.active : ""}`}>Technologies</button>
+                    </div>
+                    <div className={`row ${styles.closeSessionContainer}`}>
+                        <button
+                            onClick={() => setSession("off")}
+                            className={`${styles.closeSessionButton} ${
+                            session === "off" ? styles.activeSession : ""}`}
+                        >Cerrar sesion</button>
+                    </div>
+                </div>
+                <div className={`col-9 ${styles.contenidoContainer}`}>
+                    <div className={`row p-3`}>
+                        <h3 className='text-white text-center'>{activeTab}</h3>
+                    </div>
+                    <div className='row flex-grow-1 align-items-center'>
+                        {activeTab === "Profile" && <ProfileAdm />}
+                        {activeTab === "Projects" && <ProjectsAdm />}
+                        {activeTab === "Studies" && <StudiesAdm />}
+                        {activeTab === "Assignatures" && <AssignaturesAdm />}
+                        {activeTab === "Technologies" && <TechAdm />}
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
